213888125b9deaf08e6f418fc20d247521fe7ced
thyoon515 <thyoon515@gmail.com>
2022-08-09 22:09:58 -0400

    commit tree f6d82bbcfecc5f3cdb837c9981a6995049ddc75c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default("Naureen")
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+sum = add(2, 5)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    return number / 2
+end
+
+result = halve(6)
